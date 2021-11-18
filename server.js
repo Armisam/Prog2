b2dba614e2f570e914b254c23c96b5e2f0187d19
Armisam <csabaszekely99@gmail.com>
2021-11-18 21:28:14 +0100

    adminadd and addtocart are ready
    Now admin users can new add items and all users can add items to their own cart and remove items

--- server.js
+++ server.js
@@ -62,8 +62,13 @@ app.post('/registry', async function (req, res){
             return res.json({ status: 'error', msg: 'User already exists!'});
           }
         }
-        formData.password = hashPassword;
-        jsonData.normalusers.push(formData);
+
+        const registeredUser = {
+          username: req.body.username,
+          password: hashPassword,
+          cart:[]
+        };
+        jsonData.normalusers.push(registeredUser);
         const parsed = JSON.stringify(jsonData, null, 2);
         fs.writeFile('assets/json/register.json', parsed, (err) => {
           if(err) console.log(err);
@@ -160,11 +165,23 @@ class item {
   }
 }
 
+app.post('/load', function (req, res){
+  fs.readFile('assets/json/register.json', 'utf8', function(err, data){
+    const jsonData = JSON.parse(data);
+    for (var i = 0; i < jsonData.adminusers.length; i++) {
+      if (jsonData.adminusers[i].username == req.body.username) {
+        return res.json(true);
+      }
+    }
+    return res.json(false);
+  });
+});
+
 app.get('/load', function (req, res){
   fs.readFile('assets/json/items.json', 'utf8', function(err, data){
-    const jsonData = JSON.parse(data);
-    var resArray = [];
 
+    var resArray = [];
+    const jsonData = JSON.parse(data);
     const total_length = Math.max(jsonData.Cameras_Photo.length,
                        jsonData.Cellphones_Accesories.length,
                        jsonData.Computers_Tablets.length,
@@ -251,6 +268,19 @@ app.get('/Cameras_Photo', function (req, res){
         jsonData.Cameras_Photo[i].picture_source
       ));
     }
+    resArray.sort((a, b) => {
+    let fa = a.product_name.toLowerCase(),
+        fb = b.product_name.toLowerCase();
+
+    if (fa < fb) {
+        return -1;
+    }
+    if (fa > fb) {
+        return 1;
+    }
+    return 0;
+    });
+
     return res.json(resArray);
   });
 });
@@ -266,6 +296,19 @@ app.get('/Cellphones_Accesories', function (req, res){
         jsonData.Cellphones_Accesories[i].picture_source
       ));
     }
+    resArray.sort((a, b) => {
+    let fa = a.product_name.toLowerCase(),
+        fb = b.product_name.toLowerCase();
+
+    if (fa < fb) {
+        return -1;
+    }
+    if (fa > fb) {
+        return 1;
+    }
+    return 0;
+    });
+
     return res.json(resArray);
   });
 });
@@ -281,6 +324,19 @@ app.get('/Computers_Tablets', function (req, res){
         jsonData.Computers_Tablets[i].picture_source
       ));
     }
+    resArray.sort((a, b) => {
+    let fa = a.product_name.toLowerCase(),
+        fb = b.product_name.toLowerCase();
+
+    if (fa < fb) {
+        return -1;
+    }
+    if (fa > fb) {
+        return 1;
+    }
+    return 0;
+    });
+
     return res.json(resArray);
   });
 });
@@ -296,6 +352,19 @@ app.get('/TV_Audio_Surveilance', function (req, res){
         jsonData.TV_Audio_Surveilance[i].picture_source
       ));
     }
+    resArray.sort((a, b) => {
+    let fa = a.product_name.toLowerCase(),
+        fb = b.product_name.toLowerCase();
+
+    if (fa < fb) {
+        return -1;
+    }
+    if (fa > fb) {
+        return 1;
+    }
+    return 0;
+    });
+
     return res.json(resArray);
   });
 });
@@ -303,7 +372,7 @@ app.get('/Vechile_Electronics_GPS', function (req, res){
   fs.readFile('assets/json/items.json', 'utf8', function(err, data){
     const jsonData = JSON.parse(data);
     var resArray = [];
-    for (var i = 0; i < jsonData.Cameras_Photo.length; i++) {
+    for (var i = 0; i < jsonData.Vechile_Electronics_GPS.length; i++) {
       resArray.push(new item(
         jsonData.Vechile_Electronics_GPS[i].product_name,
         jsonData.Vechile_Electronics_GPS[i].price,
@@ -311,6 +380,19 @@ app.get('/Vechile_Electronics_GPS', function (req, res){
         jsonData.Vechile_Electronics_GPS[i].picture_source
       ));
     }
+    resArray.sort((a, b) => {
+    let fa = a.product_name.toLowerCase(),
+        fb = b.product_name.toLowerCase();
+
+    if (fa < fb) {
+        return -1;
+    }
+    if (fa > fb) {
+        return 1;
+    }
+    return 0;
+    });
+
     return res.json(resArray);
   });
 });
@@ -326,7 +408,274 @@ app.get('/Video_Games_Consoles', function (req, res){
         jsonData.Video_Games_Consoles[i].picture_source
       ));
     }
+    resArray.sort((a, b) => {
+    let fa = a.product_name.toLowerCase(),
+        fb = b.product_name.toLowerCase();
+
+    if (fa < fb) {
+        return -1;
+    }
+    if (fa > fb) {
+        return 1;
+    }
+    return 0;
+    });
+
     return res.json(resArray);
+  });
+});
+
+app.post('/itemadd', function (req, res){
+  fs.readFile('assets/json/items.json', 'utf8', function(err, data){
+    const jsonData = JSON.parse(data);
+    const addedItem = {
+      product_name: req.body.product_name,
+      picture_source: req.body.picture_source,
+      price: req.body.price,
+      description: req.body.description,
+      category: req.body.category
+    }
+
+    switch (addedItem.category) {
+      case 'Cameras_Photo':
+        jsonData.Cameras_Photo.push(addedItem);
+        break;
+      case 'Cellphones_Accesories':
+        jsonData.Cellphones_Accesories.push(addedItem);
+        break;
+      case 'Computers_Tablets':
+        jsonData.Computers_Tablets.push(addedItem);
+        break;
+      case 'TV_Audio_Surveilance':
+        jsonData.TV_Audio_Surveilance.push(addedItem);
+        break;
+      case 'Vechile_Electronics_GPS':
+        jsonData.Vechile_Electronics_GPS.push(addedItem);
+        break;
+
+      default:
+      jsonData.Video_Games_Consoles.push(addedItem);
+    }
+
+    const parsed = JSON.stringify(jsonData, null, 2);
+    fs.writeFile('assets/json/items.json', parsed, (err) => {
+      if(err) console.log(err);
+      console.log('Item added!');
+      return res.json({ status: 'ok', msg: 'Item added!'});
+    });
+  });
+});
+
+app.post('/addToCart', function (req, res){
+var category = '';
+  fs.readFile('assets/json/items.json', 'utf8', function(err, data){
+    const jsonData = JSON.parse(data);
+    const itemToSplice = req.body.product_name;
+    const total_length = Math.max(jsonData.Cameras_Photo.length,
+                       jsonData.Cellphones_Accesories.length,
+                       jsonData.Computers_Tablets.length,
+                       jsonData.TV_Audio_Surveilance.length,
+                       jsonData.Vechile_Electronics_GPS.length,
+                       jsonData.Video_Games_Consoles.length);
+
+    for (var i = 0; i < total_length; i++) {
+      if (i < jsonData.Cameras_Photo.length &&
+      itemToSplice == jsonData.Cameras_Photo[i].product_name) {
+        category = 'Cameras_Photo';
+        jsonData.Cameras_Photo.splice(i, 1);
+        i = total_length;
+      } else if (i < jsonData.Cellphones_Accesories.length &&
+      itemToSplice == jsonData.Cellphones_Accesories[i].product_name) {
+        category = 'Cellphones_Accesories';
+        jsonData.Cellphones_Accesories.splice(i, 1);
+        i = total_length;
+      } else if (i < jsonData.Computers_Tablets.length &&
+      itemToSplice == jsonData.Computers_Tablets[i].product_name) {
+        category = 'Computers_Tablets';
+        jsonData.Computers_Tablets.splice(i, 1);
+        i = total_length;
+      } else if (i < jsonData.TV_Audio_Surveilance.length &&
+      itemToSplice == jsonData.TV_Audio_Surveilance[i].product_name) {
+        category = 'TV_Audio_Surveilance';
+        jsonData.TV_Audio_Surveilance.splice(i, 1);
+        i = total_length;
+      } else if (i < jsonData.Vechile_Electronics_GPS.length &&
+      itemToSplice == jsonData.Vechile_Electronics_GPS[i].product_name) {
+        category = 'Vechile_Electronics_GPS';
+        jsonData.Vechile_Electronics_GPS.splice(i, 1);
+        i = total_length;
+      } else if (i < jsonData.Video_Games_Consoles.length &&
+      itemToSplice == jsonData.Video_Games_Consoles[i].product_name) {
+        category = 'Video_Games_Consoles';
+        jsonData.Video_Games_Consoles.splice(i, 1);
+        i = total_length;
+      }
+    }
+
+    const parsed = JSON.stringify(jsonData, null, 2);
+    fs.writeFile('assets/json/items.json', parsed, (err) => {
+      if(err) console.log(err);
+    });
+
+    fs.readFile('assets/json/register.json', 'utf8', function(err, data){
+      const jsonData = JSON.parse(data);
+      const username = req.body.username;
+      const total_length = Math.max(jsonData.normalusers.length,
+                         jsonData.adminusers.length);
+
+
+      for (var i = 0; i < total_length; i++) {
+        if (i < jsonData.normalusers.length &&
+        username == jsonData.normalusers[i].username) {
+          jsonData.normalusers[i].cart.push(JSON.parse(JSON.stringify({product_name: req.body.product_name,
+          price: req.body.price, description: req.body.description, picture_source: req.body.picture_source, category: category})));
+        } else if (i < jsonData.adminusers.length &&
+        username == jsonData.adminusers[i].username) {
+          jsonData.adminusers[i].cart.push(JSON.parse(JSON.stringify({product_name: req.body.product_name,
+          price: req.body.price, description: req.body.description, picture_source: req.body.picture_source, category: category})));
+        }
+      }
+
+
+      const parsed = JSON.stringify(jsonData, null, 2);
+      fs.writeFile('assets/json/register.json', parsed, (err) => {
+        if(err) console.log(err);
+          console.log('Item added to cart!');
+          return res.json({ status: 'ok', msg: 'Item added to cart!'});
+      });
+    });
+  });
+});
+
+app.post('/showCart', function (req,res){
+  fs.readFile('assets/json/register.json', 'utf8', function(err, data){
+
+    var resArray = [];
+    const jsonData = JSON.parse(data);
+
+    const username = req.body.username;
+    const total_length = Math.max(jsonData.normalusers.length,
+                       jsonData.adminusers.length);
+
+    for (var i = 0; i < total_length; i++) {
+      if (i < jsonData.normalusers.length &&
+      username == jsonData.normalusers[i].username) {
+        const cart = jsonData.normalusers[i].cart;
+        if (cart.length == 0) {
+          return res.json({ status: 'error', msg: 'Your cart is empty!'});
+        }
+        for (var j = 0; j < cart.length; j++) {
+          resArray.push(cart[j]);
+        }
+      } else if (i < jsonData.adminusers.length &&
+      username == jsonData.adminusers[i].username) {
+        const cart = jsonData.adminusers[i].cart;
+        if (cart.length == 0) {
+          return res.json({ status: 'error', msg: 'Your cart is empty!'});
+        }
+        for (var j = 0; j < cart.length; j++) {
+          resArray.push(cart[j]);
+        }
+      }
+    }
+      return res.json(resArray);
+  });
+});
+
+app.post('/removeFromCart', function (req, res){
+    fs.readFile('assets/json/register.json', 'utf8', function(err, data){
+      const jsonData = JSON.parse(data);
+      const username = req.body.username;
+      const product_name = req.body.product_name;
+      let category = '';
+      const total_length = Math.max(jsonData.normalusers.length,
+                         jsonData.adminusers.length);
+
+
+      for (var i = 0; i < total_length; i++) {
+         if (i < jsonData.normalusers.length &&
+         username == jsonData.normalusers[i].username) {
+           const cart = jsonData.normalusers[i].cart;
+           if (cart.length == 0) {
+             return res.json({ status: 'error', msg: 'Your cart is empty!'});
+           }
+           for (var j = 0; j < cart.length; j++) {
+             if (cart[j].product_name == product_name) {
+                 category = cart[j].category;
+                jsonData.normalusers[i].cart.splice(j, 1);
+                j = cart.length;
+                i = total_length;
+             }
+           }
+         } else if (i < jsonData.adminusers.length &&
+           username == jsonData.adminusers[i].username) {
+             const cart = jsonData.adminusers[i].cart;
+             if (cart.length == 0) {
+               return res.json({ status: 'error', msg: 'Your cart is empty!'});
+             }
+             for (var j = 0; j < cart.length; j++) {
+               if (cart[j].product_name == product_name) {
+                  category = cart[j].category;
+                  jsonData.adminusers[i].cart.splice(j, 1);
+                  j = cart.length;
+                  i = total_length;
+               }
+             }
+          }
+       }
+
+
+      const parsed = JSON.stringify(jsonData, null, 2);
+      fs.writeFile('assets/json/register.json', parsed, (err) => {
+        if(err) console.log(err);
+      });
+
+      fs.readFile('assets/json/items.json', 'utf8', function(err, data){
+        const jsonData = JSON.parse(data);
+        const itemToAdd = req.body.product_name;
+        const total_length = Math.max(jsonData.Cameras_Photo.length,
+                           jsonData.Cellphones_Accesories.length,
+                           jsonData.Computers_Tablets.length,
+                           jsonData.TV_Audio_Surveilance.length,
+                           jsonData.Vechile_Electronics_GPS.length,
+                           jsonData.Video_Games_Consoles.length);
+
+        for (var i = 0; i < total_length; i++) {
+          if (i < jsonData.Cameras_Photo.length && category == 'Cameras_Photo') {
+            jsonData.Cameras_Photo.push(JSON.parse(JSON.stringify({product_name: req.body.product_name,
+              price: req.body.price, description: req.body.description, picture_source: req.body.picture_source})));
+            i = total_length;
+          } else if (i < jsonData.Cellphones_Accesories.length && category == 'Cellphones_Accesories') {
+            jsonData.Cellphones_Accesories.push(JSON.parse(JSON.stringify({product_name: req.body.product_name,
+              price: req.body.price, description: req.body.description, picture_source: req.body.picture_source})));
+            i = total_length;
+          } else if (i < jsonData.Computers_Tablets.length && category == 'Cellphones_Accesories') {
+            jsonData.Computers_Tablets.push(JSON.parse(JSON.stringify({product_name: req.body.product_name,
+              price: req.body.price, description: req.body.description, picture_source: req.body.picture_source})));
+            i = total_length;
+          } else if (i < jsonData.TV_Audio_Surveilance.length && category == 'TV_Audio_Surveilance') {
+            jsonData.TV_Audio_Surveilance.push(JSON.parse(JSON.stringify({product_name: req.body.product_name,
+              price: req.body.price, description: req.body.description, picture_source: req.body.picture_source})));
+            i = total_length;
+            console.log(jsonData.product_name);
+          } else if (i < jsonData.Vechile_Electronics_GPS.length && category == 'Vechile_Electronics_GPS') {
+            jsonData.Vechile_Electronics_GPS.push(JSON.parse(JSON.stringify({product_name: req.body.product_name,
+              price: req.body.price, description: req.body.description, picture_source: req.body.picture_source})));
+            i = total_length;
+          } else if (i < jsonData.Video_Games_Consoles.length && category == 'Video_Games_Consoles') {
+            jsonData.Video_Games_Consoles.push(JSON.parse(JSON.stringify({product_name: req.body.product_name,
+              price: req.body.price, description: req.body.description, picture_source: req.body.picture_source})));
+            i = total_length;
+          }
+        }
+
+      const parsed = JSON.stringify(jsonData, null, 2);
+      fs.writeFile('assets/json/items.json', parsed, (err) => {
+        if(err) console.log(err);
+        console.log('Item removed from cart!');
+        return res.json({ status: 'ok', msg: 'Item removed from cart!'});
+      });
+    });
   });
 });
 
